6051d6040ac9336f3431ad56819b68bc0dc332a9
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-04-21 18:41:02 -0700

    I have the css refactored to jquery, checked to come.

--- README.md
+++ README.md
@@ -23,3 +23,5 @@ HTML5, CSS3, JS, Jquery.
 https://www.w3schools.com/html/html_forms.asp
 https://css-tricks.com/css-sprites/
 https://stackoverflow.com/questions/4137255/checkboxes-in-web-pages-how-to-make-them-bigger?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa 
+https://www.w3schools.com/jquery/jquery_css.asp
+https://www.w3schools.com/jquery/event_hover.asp
--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 'use strict';
+
+$('html').css({'width': '1000px'});
+$('h1').css({'margin-left': '250px',
+  'display': 'block',
+  'text-decoration': 'underline'});
+
+$('body').css({'display': 'block',
+  'position': 'relative',
+  'padding': '20px',
+  'margin-left': '40px'});
+
+$('input[type=checkbox]').css({'display': 'none'});
+$('input[type=radio]').css({'display': 'none'});
+
+$('input[type="checkbox"] + label.checkbox').css({'display':'inline-block',
+  'cursor': 'pointer',
+  'outline': '0',
+  'width': '80px',
+  'height': '80px',
+  'background-image': 'url("form-elements-sprite.png")',
+  'background-repeat': 'no-repeat'});
+
+$('#checkBox input[type="checkbox"]:checked + label.checkbox:after').css({'background-position': '-100 bottom'});
+
+$('input[type="radio"] + label.radio').css({'display':'inline-block',
+  'cursor': 'pointer',
+  'outline': '0',
+  'width': '200px',
+  'height': '200px',
+  'background-image': 'url("form-elements-sprite.png")',
+  'background-repeat': 'no-repeat',
+  'background-position': '-420 0'});
+
+$('input[type="radio"]:checked + label.radio:after').css({'background-position': '-500 bottom'});
+
+$('button').css({'height': '60px',
+  'width': '100px'});
+
+$('button').hover(function(){
+  $(this).css({'background-color': 'darkslategrey'});
+},
+function(){
+  $(this).css({'background-color': 'lightgrey'});
+}
+);
